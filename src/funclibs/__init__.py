2937bd7b5348625f320a1c256a86783bc533ccb8
Johannes <johannes.programming@gmail.com>
2026-01-07 05:45:37 +0100

    Version 0.0.0.dev0

---
+++ src/funclibs/__init__.py
@@ -0,0 +1,5 @@
+from funclibs.core import *
+from funclibs.tests import *
+
+if __name__ == "__main__":
+    main()
---
+++ src/funclibs/__main__.py
@@ -0,0 +1,4 @@
+from funclibs import main
+
+if __name__ == "__main__":
+    main()
